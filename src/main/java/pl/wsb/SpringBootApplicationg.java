62f8a11d8ec209c32463829ba57deef414d381a3
kacpersukiennik <kacsuk21@gmail.com>
2026-03-28 19:42:40 +0100

    Fix application main class configuration

--- src/main/java/pl/wsb/SpringBootApplicationg.java
+++
@@ -1,4 +0,0 @@
-package pl.wsb;
-
-public @interface SpringBootApplicationg {
-}
---
+++ src/main/java/pl/wsb/fitnesstracker/FitnessTracker.java
@@ -0,0 +1,12 @@
+package pl.wsb.fitnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTracker {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTracker.class, args);
+    }
+}
