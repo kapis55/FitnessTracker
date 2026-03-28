80f2d56280b418004db2846b7d620b872a777fb3
kacpersukiennik <kacsuk21@gmail.com>
2026-03-28 18:59:11 +0100

    Dodawanie statystyk do encji

---
+++ src/main/java/pl/wsb/fitnesstracker/statistics/Statistics.java
@@ -0,0 +1,55 @@
+package pl.wsb.fitnesstracker.statistics;
+
+import jakarta.persistence.*;
+import pl.wsb.fitnesstracker.user.api.User;
+
+@Entity
+@Table(name = "statistics")
+public class Statistics {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+
+  @OneToOne
+  @JoinColumn(name = "user_id", nullable = false, unique = true)
+  private User user;
+
+  @Column(nullable = false)
+  private Double totalDistance;
+
+  @Column(nullable = false)
+  private Double totalCalories;
+
+  public Statistics() {
+  }
+
+  public Long getId() {
+    return id; // 🔥 MUSI BYĆ
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Double getTotalDistance() {
+    return totalDistance;
+  }
+
+  public void setTotalDistance(Double totalDistance) {
+    this.totalDistance = totalDistance;
+  }
+
+  public Double getTotalCalories() {
+    return totalCalories;
+  }
+
+  public void setTotalCalories(Double totalCalories) {
+    this.totalCalories = totalCalories;
+  }
+}
